1b48ce3d275cc5f8a84af6ed1e8b672d0c847fef
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-03-21 14:30:12 -0600

    Refactor: se agrego etiqueta details junto con summary

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const checkboxes = document.querySelectorAll('#escenario-prueba-indice input[type=checkbox]');
+const checkboxesMain = document.querySelectorAll('main input[type=checkbox]');
 
 if (!checkboxes) {
 	console.error('No se encontraron elementos checkboxes');
@@ -20,14 +21,37 @@ checkboxes.forEach((checkbox) => {
 	const assignedStatus = () => {
 		// Cambiar el estado
 		if (currentState === 'default') {
-			currentState = 'correct';
-		} else if (currentState === 'correct') {
-			currentState = 'fail';
+			currentState = 'susses';
+		} else if (currentState === 'susses') {
+			currentState = 'error';
 		} else {
 			currentState = 'default';
 		}
 
-		console.log('estado:', currentState);
+		// Actualizar el estado en la interfaz
+		statusLabel.innerHTML = statusText[currentState];
+	};
+
+	// Actualizar el estado en la interfaz
+	statusLabel.innerHTML = statusText[status];
+});
+
+checkboxesMain.forEach((checkbox) => {
+	let currentState = 'default'; // Estado inicial
+	const statusLabel = checkbox.parentElement?.querySelector('.mark');
+	const status = checkbox.dataset.sts || 'default'; // Estado actual del checkbox
+
+	checkbox.addEventListener('change', () => assignedStatus());
+
+	const assignedStatus = () => {
+		// Cambiar el estado
+		if (currentState === 'default') {
+			currentState = 'susses';
+		} else if (currentState === 'susses') {
+			currentState = 'error';
+		} else {
+			currentState = 'default';
+		}
 
 		// Actualizar el estado en la interfaz
 		statusLabel.innerHTML = statusText[currentState];
